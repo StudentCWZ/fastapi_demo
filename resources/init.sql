36e5fd9f9a9d3fc0d7bc5e1de6a3626dbbf2e9b8
StudentCWZ <330459539@qq.com>
2023-05-08 15:19:48 +0800

    feat: Add resources

---
+++ resources/init.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server Type    : MySQL
+
+ Date: 04/27/2023 15:52:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- -------------------------------------
+-- Create database of app
+-- -------------------------------------
+CREATE DATABASE IF NOT EXISTS app;
+
+-- -------------------------------------
+-- Select database of app
+-- -------------------------------------
+USE app;

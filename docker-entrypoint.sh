4c18c5341975622edbb26c6409196f1f8ca01f6f
StudentCWZ <330459539@qq.com>
2023-05-08 16:08:36 +0800

    feat: Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-###
- # @Software: Visual Studio Code
- # @Author: StudentCWZ
- # @Email: StudentCWZ@outlook.com
- # @Date: 2023-05-08 14:25:30
- # @Last Modified by: StudentCWZ
- # @Description:
-###
+
 set -e
 
 # shellcheck disable=SC2046
 . $(poetry env info --path)/bin/activate
 
-exec app server
+exec app migrate -- upgrade head && app server
